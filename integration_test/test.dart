9f52a91f3a7d065b13d7c655a32458aa2328eeeb
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-25 20:47:01 -0700

    removed for now

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -5,7 +5,6 @@
 
 import 'package:sos/main.dart';
 import 'package:sos/utils/export.dart';
-import 'package:sos/widgets/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -32,52 +31,16 @@ void main() async {
     preferences: prefs,
     defaults: testConfig,
   );
-  
+
   // Run the tests //
 
   group(
     'Generated tests',
     () {
       testWidgets('Test randomizer', (WidgetTester tester) async {
-        // Load localization(s) //
-
-        ezLog('Loading localizations');
-        final EFUILang l10n = await EFUILang.delegate.load(english);
-
-        // Load the app //
-
         ezLog('Loading Sos');
         await tester.pumpWidget(const Sos());
         await tester.pumpAndSettle();
-
-        // Randomize the settings //
-
-        // Open the settings menu
-        await ezTouch(tester, find.byIcon(Icons.more_vert));
-
-        // Go to the settings page
-        await ezTouchText(tester, l10n.ssPageTitle);
-
-        // Randomize the settings
-        await ezTouchText(tester, l10n.ssRandom);
-        await ezTouchText(tester, l10n.gYes);
-
-        // Return to home screen
-        await ezTapBack(tester, l10n.gBack);
-      });
-
-      testWidgets('Test CountFAB', (WidgetTester tester) async {
-        // Re-load the app //
-
-        ezLog('Loading Sos');
-        await tester.pumpWidget(const Sos());
-        await tester.pumpAndSettle();
-
-        // ♫ It's as Ez as... ♫ //
-
-        await ezTouch(tester, find.byType(CountFAB)); // 1
-        await ezTouch(tester, find.byType(CountFAB)); // 2
-        await ezTouch(tester, find.byType(CountFAB)); // 3
       });
     },
   );
