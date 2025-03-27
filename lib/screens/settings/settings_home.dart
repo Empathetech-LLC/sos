5c3aa46980b34a3eb39fa9e934f8001a53d3cdae
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-26 18:27:26 -0700

    added translations

--- lib/screens/settings/settings_home.dart
+++ lib/screens/settings/settings_home.dart
@@ -4,6 +4,7 @@
  */
 
 import '../../screens/export.dart';
+import '../../utils/export.dart';
 import '../../widgets/export.dart';
 
 import 'package:flutter/material.dart';
@@ -12,22 +13,30 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 class SettingsHomeScreen extends StatelessWidget {
   const SettingsHomeScreen({super.key});
 
+  static const EzSpacer spacer = EzSpacer();
+
   @override
-  Widget build(BuildContext context) => SosScaffold(
-        title: EFUILang.of(context)!.ssPageTitle,
-        showSettings: false,
-        body: const EzSettingsHome(
-          notFun: true,
-          additionalSettings: <Widget>[
-            EzText('Notify EMC on open'),
-            EzSpacer(),
-            EzText('Notify EMC on close'),
-          ],
-          textSettingsPath: textSettingsPath,
-          layoutSettingsPath: layoutSettingsPath,
-          colorSettingsPath: colorSettingsPath,
-          imageSettingsPath: null,
-          allowRandom: false,
-        ),
-      );
+  Widget build(BuildContext context) {
+    final Lang l10n = Lang.of(context)!;
+
+    return SosScaffold(
+      title: EFUILang.of(context)!.ssPageTitle,
+      showSettings: false,
+      body: EzSettingsHome(
+        notFun: true,
+        additionalSettings: <Widget>[
+          EzText(l10n.ssNotifyOnOpen),
+          spacer,
+          EzText(l10n.ssNotifyOnClose),
+          spacer,
+          EzText(l10n.ssNotifyOnSOS),
+        ],
+        textSettingsPath: textSettingsPath,
+        layoutSettingsPath: layoutSettingsPath,
+        colorSettingsPath: colorSettingsPath,
+        imageSettingsPath: null,
+        allowRandom: false,
+      ),
+    );
+  }
 }
