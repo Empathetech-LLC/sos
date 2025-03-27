f40e7d9a0394b0b9682a146e858923a4a9c0dd67
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-26 20:41:03 -0700

    split file

---
+++ lib/utils/location.dart
@@ -0,0 +1,24 @@
+/* sos
+ * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:geolocator/geolocator.dart';
+
+Future<String> getCoordinates() async {
+  final bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) return 'Cannot access location (disabled)';
+
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return 'Cannot access location (denied)';
+    }
+  } else if (permission == LocationPermission.deniedForever) {
+    return 'Cannot access location (denied)';
+  }
+
+  final Position pos = await Geolocator.getCurrentPosition();
+  return pos.toString();
+}
