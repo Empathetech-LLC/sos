76df125b4d9ded6b381307f9ade8b19f4535d352
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-28 13:22:30 -0700

    split settings

--- lib/screens/export.dart
+++ lib/screens/export.dart
@@ -9,15 +9,19 @@ export 'error.dart';
 export 'home.dart';
 
 export 'settings/settings_home.dart';
+
+export 'settings/ez_settings_home.dart';
 export 'settings/text_settings.dart';
 export 'settings/layout_settings.dart';
 export 'settings/color_settings.dart';
-
 
 // Route names //
 
 /// 'settings-home'
 const String settingsHomePath = 'settings-home';
+
+/// 'ez-settings-home'
+const String ezSettingsHomePath = 'ez-settings-home';
 
 /// 'text-settings'
 const String textSettingsPath = 'text-settings';
@@ -27,5 +31,3 @@ const String layoutSettingsPath = 'layout-settings';
 
 /// 'color-settings'
 const String colorSettingsPath = 'color-settings';
-
-
