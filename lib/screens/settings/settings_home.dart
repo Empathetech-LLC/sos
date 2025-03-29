462711569c99c43204820cc5d30323f9e108def8
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-28 17:01:53 -0700

    added appearance route

--- lib/screens/settings/settings_home.dart
+++ lib/screens/settings/settings_home.dart
@@ -3,9 +3,12 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'package:sos/screens/export.dart';
+
 import '../../widgets/export.dart';
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class SettingsHomeScreen extends StatelessWidget {
@@ -15,8 +18,17 @@ class SettingsHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SosScaffold(
       title: EFUILang.of(context)!.ssPageTitle,
-      showSettings: false,
-      body: const SizedBox.shrink(),
+      body: EzScreen(
+        child: EzScrollView(
+          children: <Widget>[
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(ezSettingsHomePath),
+              icon: EzIcon(Icons.navigate_next),
+              label: 'Appearance',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
