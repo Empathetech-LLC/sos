190edd393e18f53455d19b732775a276dbc4f66f
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-30 17:35:15 -0700

    split rights widget to own file

---
+++ lib/widgets/rights_view.dart
@@ -0,0 +1,126 @@
+/* sos
+ * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../utils/export.dart';
+
+import 'package:flutter/material.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+enum Location { walking, driving, home }
+
+extension Symbol on Location {
+  IconData get icon {
+    switch (this) {
+      case Location.walking:
+        return Icons.directions_walk;
+      case Location.driving:
+        return Icons.car_rental;
+      case Location.home:
+        return Icons.car_rental;
+    }
+  }
+}
+
+class RightsView extends StatefulWidget {
+  const RightsView({super.key});
+
+  @override
+  State<RightsView> createState() => _PulsingIconWidgetState();
+}
+
+class _PulsingIconWidgetState extends State<RightsView> {
+  // Gather the theme data //
+
+  static const EzSpacer spacer = EzSpacer();
+
+  late final Lang l10n = Lang.of(context)!;
+  late final TextTheme textTheme = Theme.of(context).textTheme;
+
+  // Define the build data //
+
+  Location currentTab = Location.walking;
+
+  // Define custom functions //
+
+  Widget populateTab() {
+    switch (currentTab) {
+      case Location.walking:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[rightsBlock(l10n.rsWLeave)],
+        );
+      case Location.driving:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            rightsBlock(l10n.rsCSearch),
+            rightsBlock(l10n.rsCPockets),
+            rightsBlock(l10n.rsCWarrant),
+            rightsBlock(l10n.rsCID),
+            rightsBlock(l10n.rsCQuestion),
+            rightsBlock(l10n.rsCLeave),
+            rightsBlock(l10n.rsCRemainSilent),
+          ],
+        );
+      case Location.home:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[rightsBlock(l10n.rsHWarrant)],
+        );
+    }
+  }
+
+  Text rightsBlock(String text) => Text(
+        '$text\n',
+        textAlign: TextAlign.center,
+        style: textTheme.bodyLarge,
+      );
+
+  // Return the build //
+
+  @override
+  Widget build(BuildContext context) {
+    return EzScrollView(
+      // Header
+      children: <Widget>[
+        Text(
+          l10n.rsGHeader,
+          textAlign: TextAlign.center,
+          style: textTheme.titleLarge,
+        ),
+        spacer,
+
+        // Switcher
+        SegmentedButton<Location>(
+          segments: <ButtonSegment<Location>>[
+            ButtonSegment<Location>(
+              value: Location.walking,
+              label: Icon(Location.walking.icon),
+            ),
+            ButtonSegment<Location>(
+              value: Location.driving,
+              label: Icon(Location.driving.icon),
+            ),
+            ButtonSegment<Location>(
+              value: Location.home,
+              label: Icon(Location.home.icon),
+            ),
+          ],
+          selected: <Location>{currentTab},
+          showSelectedIcon: false,
+          onSelectionChanged: (Set<Location> selected) =>
+              setState(() => currentTab = selected.first),
+        ),
+        spacer,
+
+        // Shared rights
+        rightsBlock(l10n.rsGHeader),
+
+        // Specific rights
+        populateTab(),
+      ],
+    );
+  }
+}
