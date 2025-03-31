a2e5fc8ff7011b92bbb88c8d00a1273b3a917501
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-30 23:00:00 -0700

    removed title on camera msg

--- lib/utils/tutorial.dart
+++ lib/utils/tutorial.dart
@@ -55,7 +55,6 @@ Future<dynamic> cameraMsg(BuildContext context) async {
   return showPlatformDialog(
     context: context,
     builder: (_) => EzAlertDialog(
-      title: Text(l10n.hsWelcome, textAlign: TextAlign.center),
       content: Text(l10n.hsCameraTutorial, textAlign: TextAlign.center),
       materialActions: materialActions,
       cupertinoActions: cupertinoActions,
