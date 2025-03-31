ac267648ab4c0ada8a83b1d2979d08120790c17a
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-30 19:46:03 -0700

    more buttons

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,67 @@
+/* sos
+ * Copyright (c) 2025 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../screens/export.dart';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class SettingsButton extends StatelessWidget {
+  final BuildContext parentContext;
+
+  /// [EzMenuButton] for opening the settings
+  const SettingsButton(this.parentContext, {super.key});
+
+  @override
+  Widget build(BuildContext context) => EzMenuButton(
+        onPressed: () => parentContext.goNamed(settingsHomePath),
+        icon: EzIcon(PlatformIcons(context).settings),
+        label: EFUILang.of(context)!.ssPageTitle,
+      );
+}
+
+class FlashButton extends StatelessWidget {
+  final CameraController camera;
+
+  /// setState((){})
+  final void stateCallback;
+
+  /// [EzIconButton] for toggling the [camera]s [FlashMode]
+  const FlashButton({
+    super.key,
+    required this.camera,
+    required this.stateCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) => EzIconButton(
+        icon: switch (camera.value.flashMode) {
+          FlashMode.off => const Icon(Icons.flash_off),
+          FlashMode.auto => const Icon(Icons.flash_auto),
+          FlashMode.always => const Icon(Icons.flash_on),
+          FlashMode.torch => const Icon(Icons.flashlight_on),
+        },
+        onPressed: () async {
+          switch (camera.value.flashMode) {
+            case FlashMode.off:
+              await camera.setFlashMode(FlashMode.auto);
+              break;
+            case FlashMode.auto:
+              await camera.setFlashMode(FlashMode.always);
+              break;
+            case FlashMode.always:
+              await camera.setFlashMode(FlashMode.off);
+              break;
+            case FlashMode.torch:
+              await camera.setFlashMode(FlashMode.off);
+              break;
+          }
+          stateCallback;
+        },
+      );
+}
