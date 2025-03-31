d414e16ee06a81eb71d6fa0dd78b4a9557209391
empathictech <105748722+empathictech@users.noreply.github.com>
2025-03-30 19:20:50 -0700

    using l10n

--- lib/utils/tutorial.dart
+++ lib/utils/tutorial.dart
@@ -3,36 +3,33 @@
  * See LICENSE for distribution and usage details.
  */
 
+import './export.dart';
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 Future<dynamic> firstEMCMsg(BuildContext context) async {
+  final Lang l10n = Lang.of(context)!;
+  final EFUILang el10n = EFUILang.of(context)!;
+
   final (
     List<EzMaterialAction> materialActions,
     List<EzCupertinoAction> cupertinoActions
   ) = ezActionPairs(
     context: context,
     onConfirm: Navigator.of(context).pop,
-    confirmMsg: EFUILang.of(context)!.gContinue,
+    confirmMsg: el10n.gContinue,
     onDeny: () => exit(1),
-    denyMsg: EFUILang.of(context)!.gCancel,
+    denyMsg: el10n.gCancel,
   );
 
   return showPlatformDialog(
     context: context,
     builder: (_) => EzAlertDialog(
-      title: const Text('Welcome to SOS', textAlign: TextAlign.center),
-      content: const Text(
-        '''This app is designed to share your evidence and location with emergency contacts in a moment of crisis.
-
-Please choose your first emergency contact.
-The contact must have a phone number.
-
-Just one for now, you can add more later.''',
-        textAlign: TextAlign.center,
-      ),
+      title: Text(l10n.hsWelcome, textAlign: TextAlign.center),
+      content: Text(l10n.hsATutorial, textAlign: TextAlign.center),
       materialActions: materialActions,
       cupertinoActions: cupertinoActions,
       needsClose: false,
