a8166a57088d0ff4a72d72aaf50a7239dc7d4edb
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-14 00:25:04 -0700

    removed unused

--- android/app/src/main/kotlin/net/empathetech/sos/MainActivity.kt
+++ android/app/src/main/kotlin/net/empathetech/sos/MainActivity.kt
@@ -1,7 +1,5 @@
 package net.empathetech.sos
 
-import android.content.Intent
-import android.app.PendingIntent
 import androidx.annotation.NonNull
 import android.telephony.SmsManager
 import io.flutter.plugin.common.MethodChannel
@@ -26,17 +24,11 @@ class MainActivity : FlutterActivity() {
   }
 
   private fun sendSMS(result: MethodChannel.Result, recipients: String, message: String) {
-    val sentIntent = PendingIntent.getBroadcast(activity, 0, Intent("SMS_SENT_ACTION"), PendingIntent.FLAG_IMMUTABLE)
     val smsManager = SmsManager.getDefault()
     val numbers = recipients.split(";")
 
     for (num in numbers) {
-      if (message.toByteArray().size > 80) {
-        val partMessage = smsManager.divideMessage(message)
-        smsManager.sendMultipartTextMessage(num, null, partMessage, null, null)
-      } else {
-        smsManager.sendTextMessage(num, null, message, sentIntent, null)
-      }
+      smsManager.sendTextMessage(num, null, message, null, null)
     }
 
     result.success("SMS Sent!")
