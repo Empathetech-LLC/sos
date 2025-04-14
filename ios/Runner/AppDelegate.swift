eb136a02a7eb6eb2487ef458852b812d92eb0bb1
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-14 11:35:56 -0700

    v1; chance of working: 5%

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import workmanager
+//import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,14 +8,41 @@ import workmanager
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let broadcastChannel = FlutterMethodChannel(name: "net.empathetech.sos/broadcast", binaryMessenger: controller.binaryMessenger)
+
+    broadcastChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      guard call.method == "sendSOS" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      self?.sendSMS(result: result)
+    })
+
+    //WorkmanagerPlugin.setPluginRegistrantCallback { registry in GeneratedPluginRegistrant.register(with: registry) }
+    //WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "net.empathetech.sos.sos_broadcast", frequency: NSNumber(value: 15 * 60))
+
     GeneratedPluginRegistrant.register(with: self)
-
-    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-      GeneratedPluginRegistrant.register(with: registry)
-    }
-
-    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "net.empathetech.sos.sos_broadcast", frequency: NSNumber(value: 15 * 60))
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func sendSMS(result: FlutterResult) {
+    _arguments = call.arguments as! [String : Any];
+    #if targetEnvironment(simulator)
+      result(FlutterError(
+          code: "device_not_supported",
+          message: "Must use hardware",
+          details: "Cannot send SMS/MMS on a Simulator"
+        )
+      )
+    #else
+      self.result = result
+      let controller = MFMessageComposeViewController()
+      controller.body = _arguments["message"] as? String
+      controller.recipients = _arguments["recipients"] as? [String]
+      controller.messageComposeDelegate = self
+      UIApplication.shared.keyWindow?.rootViewController?.present(controller, animated: true, completion: nil)
+    #endif
   }
 }
