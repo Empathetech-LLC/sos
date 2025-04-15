a77aa237f91ca2882300aef1085184af6c9d6bc7
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-15 15:51:44 -0700

    only one!

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -42,13 +42,13 @@ import MessageUI
         // If there is already a compose view, dismiss it
         existing.dismiss(animated: true) { [weak self] in
           self?.presentSMS(message: message, recipients: recipients, on: viewControl)
-          result(String("SMS_SUCCESS"))
         }
       } else {
         presentSMS(message: message, recipients: recipients, on: viewControl)
-        result(String("SMS_SUCCESS"))
       }
     #endif
+
+    result(String("SMS_SUCCESS"))
   }
 
   private func presentSMS(message: String, recipients: [String], on viewControl: FlutterViewController) {
