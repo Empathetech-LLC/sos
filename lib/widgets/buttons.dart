1858c33f0187e0428a384596a397c20f53c69376
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-16 22:47:25 -0700

    localized

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -3,30 +3,11 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../screens/export.dart';
+import '../utils/export.dart';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-
-class SettingsButton extends StatelessWidget {
-  final BuildContext parentContext;
-
-  /// [EzMenuButton] for opening the settings
-  const SettingsButton(this.parentContext, {super.key});
-
-  @override
-  Widget build(BuildContext context) => EzMenuButton(
-        onPressed: () => parentContext.goNamed(settingsHomePath),
-        icon: EzIcon(
-          PlatformIcons(context).settings,
-          semanticLabel: 'Settings',
-        ),
-        label: EFUILang.of(context)!.ssPageTitle,
-      );
-}
 
 class FlashButton extends StatelessWidget {
   final CameraController camera;
@@ -42,41 +23,45 @@ class FlashButton extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => EzIconButton(
-        icon: switch (camera.value.flashMode) {
-          FlashMode.off => const Icon(
-              Icons.flash_off,
-              semanticLabel: 'Off',
-            ),
-          FlashMode.auto => const Icon(
-              Icons.flash_auto,
-              semanticLabel: 'Auto',
-            ),
-          FlashMode.always => const Icon(
-              Icons.flash_on,
-              semanticLabel: 'Always',
-            ),
-          FlashMode.torch => const Icon(
-              Icons.flashlight_on,
-              semanticLabel: 'Torch',
-            ),
-        },
-        onPressed: () async {
-          switch (camera.value.flashMode) {
-            case FlashMode.off:
-              await camera.setFlashMode(FlashMode.auto);
-              break;
-            case FlashMode.auto:
-              await camera.setFlashMode(FlashMode.always);
-              break;
-            case FlashMode.always:
-              await camera.setFlashMode(FlashMode.off);
-              break;
-            case FlashMode.torch:
-              await camera.setFlashMode(FlashMode.off);
-              break;
-          }
-          stateCallback.call();
-        },
-      );
+  Widget build(BuildContext context) {
+    final Lang l10n = Lang.of(context)!;
+
+    return EzIconButton(
+      icon: switch (camera.value.flashMode) {
+        FlashMode.off => Icon(
+            Icons.flash_off,
+            semanticLabel: l10n.hsFlashOff,
+          ),
+        FlashMode.auto => Icon(
+            Icons.flash_auto,
+            semanticLabel: l10n.hsFlashAuto,
+          ),
+        FlashMode.always => Icon(
+            Icons.flash_on,
+            semanticLabel: l10n.hsFlashOn,
+          ),
+        FlashMode.torch => Icon(
+            Icons.flashlight_on,
+            semanticLabel: l10n.hsFlashTorch,
+          ),
+      },
+      onPressed: () async {
+        switch (camera.value.flashMode) {
+          case FlashMode.off:
+            await camera.setFlashMode(FlashMode.auto);
+            break;
+          case FlashMode.auto:
+            await camera.setFlashMode(FlashMode.always);
+            break;
+          case FlashMode.always:
+            await camera.setFlashMode(FlashMode.off);
+            break;
+          case FlashMode.torch:
+            await camera.setFlashMode(FlashMode.off);
+            break;
+        }
+        stateCallback.call();
+      },
+    );
+  }
 }
