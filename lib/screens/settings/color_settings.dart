a8acdb4e7ac63bf1c09f9bfb5742d1ef118db7be
empathictech <105748722+empathictech@users.noreply.github.com>
2025-04-20 14:39:42 -0700

    using new setting types

--- lib/screens/settings/color_settings.dart
+++ lib/screens/settings/color_settings.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class ColorSettingsScreen extends StatelessWidget {
-  final EzSettingType? target;
+  final EzCSType? target;
 
   const ColorSettingsScreen({super.key, this.target});
 
