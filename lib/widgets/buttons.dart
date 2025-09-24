031d6b159afed6dd30795bdf21c134c9ff1c7c2d
empathictech <105748722+empathictech@users.noreply.github.com>
2025-09-23 22:50:30 -0700

    cleanup

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -12,15 +12,8 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 class FlashButton extends StatelessWidget {
   final CameraController camera;
 
-  /// setState((){})
-  final void Function() stateCallback;
-
   /// [EzIconButton] for toggling the [camera]s [FlashMode]
-  const FlashButton({
-    super.key,
-    required this.camera,
-    required this.stateCallback,
-  });
+  const FlashButton(this.camera, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,39 +21,32 @@ class FlashButton extends StatelessWidget {
 
     return EzIconButton(
       icon: switch (camera.value.flashMode) {
-        FlashMode.off => Icon(
-            Icons.flash_off,
-            semanticLabel: l10n.hsFlashOff,
-          ),
-        FlashMode.auto => Icon(
-            Icons.flash_auto,
-            semanticLabel: l10n.hsFlashAuto,
-          ),
-        FlashMode.always => Icon(
-            Icons.flash_on,
-            semanticLabel: l10n.hsFlashOn,
-          ),
-        FlashMode.torch => Icon(
-            Icons.flashlight_on,
-            semanticLabel: l10n.hsFlashTorch,
-          ),
+        FlashMode.off => Icon(Icons.flash_off, semanticLabel: l10n.hsFlashOff),
+        FlashMode.auto =>
+          Icon(Icons.flash_auto, semanticLabel: l10n.hsFlashAuto),
+        FlashMode.always => Icon(Icons.flash_on, semanticLabel: l10n.hsFlashOn),
+        FlashMode.torch =>
+          Icon(Icons.flashlight_on, semanticLabel: l10n.hsFlashTorch),
       },
       onPressed: () async {
-        switch (camera.value.flashMode) {
-          case FlashMode.off:
-            await camera.setFlashMode(FlashMode.auto);
-            break;
-          case FlashMode.auto:
-            await camera.setFlashMode(FlashMode.always);
-            break;
-          case FlashMode.always:
-            await camera.setFlashMode(FlashMode.off);
-            break;
-          case FlashMode.torch:
-            await camera.setFlashMode(FlashMode.off);
-            break;
+        try {
+          switch (camera.value.flashMode) {
+            case FlashMode.off:
+              await camera.setFlashMode(FlashMode.auto);
+              break;
+            case FlashMode.auto:
+              await camera.setFlashMode(FlashMode.always);
+              break;
+            case FlashMode.always:
+              await camera.setFlashMode(FlashMode.torch);
+              break;
+            case FlashMode.torch:
+              await camera.setFlashMode(FlashMode.off);
+              break;
+          }
+        } catch (e) {
+          ezLog('Flash error: $e');
         }
-        stateCallback.call();
       },
     );
   }
