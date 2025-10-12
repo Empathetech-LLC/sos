4e9f29f04f064898023e29191263e185999203a4
empathictech <105748722+empathictech@users.noreply.github.com>
2025-10-12 08:26:08 -0700

    removed background image, added text opacity mirror

--- lib/screens/settings/design_settings.dart
+++ lib/screens/settings/design_settings.dart
@@ -8,22 +8,128 @@ import '../../widgets/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class DesignSettingsScreen extends StatelessWidget {
   const DesignSettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => SosScaffold(
-        const EzScreen(EzDesignSettings(), useImageDecoration: false),
-        fabs: <Widget>[
-          ezSpacer,
-          EzConfigFAB(
-            context,
-            appName: appName,
-            androidPackage: packageName,
-          ),
-          ezSpacer,
-          EzBackFAB(context),
-        ],
-      );
+  Widget build(BuildContext context) {
+    final EFUILang l10n = ezL10n(context);
+
+    return SosScaffold(
+      EzScreen(
+        EzDesignSettings(
+          includeBackgroundImage: false,
+          themedSettingsPrepend: <Widget>[
+            // Text Background Opacity mirror
+            EzElevatedIconButton(
+              onPressed: () => showModalBottomSheet(
+                  context: context,
+                  useSafeArea: true,
+                  isScrollControlled: true,
+                  constraints: const BoxConstraints(minWidth: double.infinity),
+                  builder: (_) {
+                    final bool isDark = isDarkTheme(context);
+                    final ColorScheme colorScheme =
+                        Theme.of(context).colorScheme;
+
+                    final String opacityKey = isDark
+                        ? darkTextBackgroundOpacityKey
+                        : lightTextBackgroundOpacityKey;
+
+                    double opacity = EzConfig.get(opacityKey);
+                    Color background =
+                        colorScheme.surface.withValues(alpha: opacity);
+
+                    return StatefulBuilder(
+                      builder: (_, StateSetter setModal) => EzScrollView(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          // Preview
+                          Container(
+                            width: double.infinity,
+                            height: heightOf(context) * 0.667,
+                            color: colorScheme.surface,
+                            child: Stack(children: <Widget>[
+                              const Center(
+                                child: EzImage(
+                                  image: AssetImage(ladyLiberty),
+                                  semanticLabel:
+                                      'A picture of the Statue of Liberty, with a preview of the rights list.', // TODO: l10n
+                                ),
+                              ),
+                              Container(
+                                height: double.infinity,
+                                width: double.infinity,
+                                color: background,
+                                child: const RightsView(),
+                              ),
+                            ]),
+                          ),
+                          ezSpacer,
+
+                          // Slider
+                          ConstrainedBox(
+                            constraints:
+                                BoxConstraints(maxWidth: ScreenSize.small.size),
+                            child: Slider(
+                              // Slider values
+                              value: opacity,
+                              min: minOpacity,
+                              max: maxOpacity,
+                              divisions: 20,
+                              label: opacity.toStringAsFixed(2),
+
+                              // Slider functions
+                              onChanged: (double value) => setModal(() {
+                                opacity = value;
+                                background = colorScheme.surface
+                                    .withValues(alpha: opacity);
+                              }),
+                              onChangeEnd: (double value) =>
+                                  EzConfig.setDouble(opacityKey, value),
+                            ),
+                          ),
+                          ezSpacer,
+
+                          // Local reset
+                          EzElevatedIconButton(
+                            onPressed: () async {
+                              await EzConfig.remove(opacityKey);
+
+                              setModal(() {
+                                opacity = EzConfig.getDefault(opacityKey);
+                                background = colorScheme.surface
+                                    .withValues(alpha: opacity);
+                              });
+                            },
+                            icon: EzIcon(PlatformIcons(context).refresh),
+                            label: l10n.gReset,
+                          ),
+                          EzSpacer(space: EzConfig.get(spacingKey) * 1.5),
+                        ],
+                      ),
+                    );
+                  }),
+              icon: const Icon(Icons.opacity),
+              label: l10n.tsTextBackground,
+            ),
+            ezSpacer,
+          ],
+        ),
+        useImageDecoration: false,
+      ),
+      fabs: <Widget>[
+        ezSpacer,
+        EzConfigFAB(
+          context,
+          appName: appName,
+          androidPackage: packageName,
+        ),
+        ezSpacer,
+        EzBackFAB(context),
+      ],
+    );
+  }
 }
