8521a41db07b7647188e2079732302feb534ecda
empathictech <105748722+empathictech@users.noreply.github.com>
2025-11-13 17:49:48 -0800

    q/a drafts

--- lib/widgets/fabulous.dart
+++ lib/widgets/fabulous.dart
@@ -11,6 +11,7 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 // TODO: l10n
+// TODO: make sure SOS on close doesn't activate for links
 
 const Widget updater = EzUpdaterFAB(
   appVersion: '1.5.1',
@@ -39,7 +40,7 @@ class HelpFAB extends StatelessWidget {
         ),
         builder: (BuildContext modalContext) => EzScrollView(
           children: <Widget>[
-            //* Expandable FAQ(s) *//
+            //* Expandable FAQ *//
 
             Text(
               'FAQ',
@@ -58,11 +59,36 @@ class HelpFAB extends StatelessWidget {
               ),
               children: <Widget>[
                 Text(
-                  'Answer',
+                  'The list of rights was compiled from several public sources, including:',
                   style: textTheme.bodyLarge,
                   textAlign: TextAlign.start,
                 ),
-              ],
+                Text(
+                  'ACLU Know Your Rights',
+                  style: textTheme.bodyLarge,
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'IMMDEF Resources',
+                  style: textTheme.bodyLarge,
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'Dunk the Vote: The Black Book',
+                  style: textTheme.bodyLarge,
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'Another useful resource:',
+                  style: textTheme.bodyLarge,
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'How to document ICE',
+                  style: textTheme.bodyLarge,
+                  textAlign: TextAlign.start,
+                ),
+              ], // TODO: Update for link(s)
             ),
 
             // Android specific //
@@ -76,7 +102,37 @@ class HelpFAB extends StatelessWidget {
                 ),
                 children: <Widget>[
                   Text(
-                    'Answer',
+                    'SOS on open: when enabled, an SOS broadcast will being immediately on app launch. Not recommended for most users, as it could lead to accidental broadcasts.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'SOS on close: when enabled, a safe close (thumbs up) button will appear on the home page, opposite the settings. If InstaSOS loses focus for any reason except the safe close button (minimized, switched apps, screen off), an SOS broadcast will begin.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ), // TODO: use the pre-existing l10n, with the new ending
+                  ezStartLine,
+                  Text(
+                    "SOS on interrupted recording: when enabled, if a video recording is active and InstaSOS loses focus for any reason (minimized, switched apps, screen off), an SOS broadcast will begin.\nIt is recommended to enable either this or 'SOS on close' (or both).",
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'Auto-share media: when enabled, the native share options will appear as soon as a video completes or a photo is taken.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'Emergency contacts: the people that will be sent your location while an SOS broadcast is active.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    "Link type: how your location will be delivered to your emergency contacts. 'Google Maps', 'Apple Maps', and 'Waze' will send a clickable link for the corresponding service. 'Coordinates' will send your latitude and longitude as plain text.",
                     style: textTheme.bodyLarge,
                     textAlign: TextAlign.start,
                   ),
@@ -92,23 +148,7 @@ class HelpFAB extends StatelessWidget {
                 ),
                 children: <Widget>[
                   Text(
-                    'Answer',
-                    style: textTheme.bodyLarge,
-                    textAlign: TextAlign.start,
-                  ),
-                ],
-              ),
-
-              // Clarity on feature disparity
-              ExpansionTile(
-                title: Text(
-                  'My friend has less options?',
-                  style: textTheme.bodyLarge,
-                  textAlign: TextAlign.start,
-                ),
-                children: <Widget>[
-                  Text(
-                    'Answer',
+                    'Answer', // TODO
                     style: textTheme.bodyLarge,
                     textAlign: TextAlign.start,
                   ),
@@ -127,7 +167,25 @@ class HelpFAB extends StatelessWidget {
                 ),
                 children: <Widget>[
                   Text(
-                    'Answer',
+                    'SOS on open: when enabled, an SOS broadcast will being immediately on app launch. Not recommended for most users, as it could lead to accidental broadcasts.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'Auto-share media: when enabled, the native share options will appear as soon as a video completes or a photo is taken.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'Emergency contacts: the people that will be sent your location while an SOS broadcast is active.',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    "Link type: how your location will be delivered to your emergency contacts. 'Google Maps', 'Apple Maps', and 'Waze' will send a clickable link for the corresponding service. 'Coordinates' will send your latitude and longitude as plain text.",
                     style: textTheme.bodyLarge,
                     textAlign: TextAlign.start,
                   ),
@@ -137,29 +195,35 @@ class HelpFAB extends StatelessWidget {
               // Clarity for platform headache(s)
               ExpansionTile(
                 title: Text(
-                  'Contact has no number/is private?',
+                  'The contact is private or does not have a phone number?',
                   style: textTheme.bodyLarge,
                   textAlign: TextAlign.start,
                 ),
                 children: <Widget>[
                   Text(
-                    'Answer',
+                    'If you are (unexpectedly) seeing this message when adding an emergency contact, it is likely InstaSOS does not have permission to access that contact.',
                     style: textTheme.bodyLarge,
                     textAlign: TextAlign.start,
-                  ),
+                  ), // TODO: Update for link(s)
                 ],
               ),
 
               // Clarity on feature disparity
               ExpansionTile(
                 title: Text(
-                  'My friend has more options?',
+                  'Why are some Android features missing?',
                   style: textTheme.bodyLarge,
                   textAlign: TextAlign.start,
                 ),
                 children: <Widget>[
                   Text(
-                    'Answer',
+                    'The missing features require SMS automation, and Apple does not allow developers to automate text messages (without a paid service).',
+                    style: textTheme.bodyLarge,
+                    textAlign: TextAlign.start,
+                  ),
+                  ezStartLine,
+                  Text(
+                    'What about iOS shortcuts?\nThere are also restrictions on accessing user shortcuts, which would break crucial features. Chiefly: it would interrupt video recordings.',
                     style: textTheme.bodyLarge,
                     textAlign: TextAlign.start,
                   ),
@@ -171,16 +235,16 @@ class HelpFAB extends StatelessWidget {
 
             ExpansionTile(
               title: Text(
-                'Why is X language not included?',
+                'Why is X language not available?',
                 style: textTheme.bodyLarge,
                 textAlign: TextAlign.start,
               ),
               children: <Widget>[
                 Text(
-                  'Answer',
+                  'It would be irresponsible to use machine translation for the rights list. Please consider contributing time and/or resources to help expand the language options.',
                   style: textTheme.bodyLarge,
                   textAlign: TextAlign.start,
-                ),
+                ), // TODO: Update for link(s)
               ],
             ),
 
