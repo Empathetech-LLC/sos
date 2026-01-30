1e7d5674815c638bf498cb497caa0cda1e6da290
empathictech <105748722+empathictech@users.noreply.github.com>
2026-01-30 14:49:45 -0500

    added video colors

--- lib/utils/sos_cache.dart
+++ lib/utils/sos_cache.dart
@@ -8,15 +8,22 @@ import './export.dart';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
+//* Cache *//
+
 class SOSCache extends EzAppCache {
   // Construct //
 
   Locale _locale;
   Lang _l10n;
 
+  late Color _videoColor;
+  late Color _videoTextColor;
+
   SOSCache(Locale locale, Lang l10n)
       : _locale = locale,
-        _l10n = l10n;
+        _l10n = l10n {
+    _setVideoColors();
+  }
 
   // Get //
 
@@ -26,19 +33,35 @@ class SOSCache extends EzAppCache {
 
   @override
   Future<void> redraw() async {
+    _setVideoColors();
     if (_locale != EzConfig.locale) {
       _l10n = await Lang.delegate.load(EzConfig.locale);
       _locale = EzConfig.locale;
     }
   }
+
+  void _setVideoColors() {
+    _videoColor =
+        EzConfig.get(EzConfig.isDark ? darkVideoColorKey : lightVideoColorKey);
+    _videoTextColor = getTextColor(_videoColor);
+  }
 }
 
-Lang get l10n => (EzConfig.appCache! as SOSCache).l10n;
+//* Aliases *//
+
+SOSCache get _cache => EzConfig.appCache! as SOSCache;
+
+Lang get l10n => _cache.l10n;
+
+bool get autoShare => EzConfig.get(autoShareMediaKey);
 
 List<String>? emc = EzConfig.get(emcKey);
+
+LinkType get linkType => LinkConfig.lookup(EzConfig.get(linkTypeKey));
+
 bool get sosOnOpen => EzConfig.get(sosOnOpenKey);
 bool get sosOnClose => EzConfig.get(sosOnCloseKey);
 bool get sosOnInterrupt => EzConfig.get(sosOnInterruptKey);
 
-bool get autoShare => EzConfig.get(autoShareMediaKey);
-LinkType get linkType => LinkConfig.lookup(EzConfig.get(linkTypeKey));
+Color get videoColor => _cache._videoColor;
+Color get videoTextColor => _cache._videoTextColor;
