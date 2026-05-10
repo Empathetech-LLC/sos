fd5ffbba0c36c7e77587ee2e9c7d112f2c45e030
empathictech <105748722+empathictech@users.noreply.github.com>
2026-05-10 09:11:55 -0400

    added ezfauxcarousel

--- lib/widgets/rights_view.dart
+++ lib/widgets/rights_view.dart
@@ -18,46 +18,27 @@ class RightsView extends StatefulWidget {
 class _RightsViewState extends State<RightsView> {
   // Define the build data //
 
-  Situation currentTab = SituationConfig.lookup(EzConfig.get(savedTabKey));
+  Situation currTab = SituationConfig.lookup(EzConfig.get(savedTabKey));
+  int delta = 0;
 
   // Define custom functions //
 
-  Widget populateTab() {
-    switch (currentTab) {
-      case Situation.walking:
-        return EzCol(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            rightsBlock(l10n.rvMobilePockets),
-            rightsBlock(l10n.rvMobileQuestion),
-            rightsBlock(l10n.rvMobileLeave),
+  List<Widget> _unique() => switch (currTab) {
+        Situation.walking => <Widget>[
+            _rightsText(l10n.rvMobilePockets),
+            _rightsText(l10n.rvMobileQuestion),
+            _rightsText(l10n.rvMobileLeave),
           ],
-        );
-      case Situation.driving:
-        return EzCol(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            rightsBlock(l10n.rvMobilePockets),
-            rightsBlock(l10n.rvMobileQuestion),
-            rightsBlock(l10n.rvMobileLeave),
-            rightsBlock(l10n.rvDriveSearch),
-            rightsBlock(l10n.rvDriveID),
-            rightsBlock(l10n.rvDriveWarrant),
+        Situation.driving => <Widget>[
+            _rightsText(l10n.rvMobilePockets),
+            _rightsText(l10n.rvMobileQuestion),
+            _rightsText(l10n.rvMobileLeave),
+            _rightsText(l10n.rvDriveSearch),
+            _rightsText(l10n.rvDriveID),
+            _rightsText(l10n.rvDriveWarrant),
           ],
-        );
-      case Situation.home:
-        return EzCol(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[rightsBlock(l10n.rvHomeWarrant)],
-        );
-    }
-  }
-
-  Text rightsBlock(String text) => Text(
-        '$text\n',
-        textAlign: TextAlign.start,
-        style: EzConfig.styles.bodyLarge,
-      );
+        Situation.home => <Widget>[_rightsText(l10n.rvHomeWarrant)],
+      };
 
   // Return the build //
 
@@ -80,46 +61,50 @@ class _RightsViewState extends State<RightsView> {
             // Switcher
             Center(
               child: SegmentedButton<Situation>(
-                segments: <ButtonSegment<Situation>>[
-                  ButtonSegment<Situation>(
-                    value: Situation.walking,
-                    label: Icon(Situation.walking.icon),
-                    tooltip: l10n.rvOnFoot,
-                  ),
-                  ButtonSegment<Situation>(
-                    value: Situation.driving,
-                    label: Icon(Situation.driving.icon),
-                    tooltip: l10n.rvWhileDriving,
-                  ),
-                  ButtonSegment<Situation>(
-                    value: Situation.home,
-                    label: Icon(Situation.home.icon),
-                    tooltip: l10n.rvAtHome,
-                  ),
-                ],
-                selected: <Situation>{currentTab},
+                segments: Situation.values
+                    .map((Situation sitch) => ButtonSegment<Situation>(
+                          value: sitch,
+                          label: Icon(sitch.icon),
+                          tooltip: sitch.tooltip,
+                        ))
+                    .toList(),
+                selected: <Situation>{currTab},
                 showSelectedIcon: false,
                 onSelectionChanged: (Set<Situation> selected) async {
-                  currentTab = selected.first;
-                  await EzConfig.setString(savedTabKey, currentTab.name);
+                  final Situation choice = selected.first;
+                  delta = choice.position - currTab.position;
+
+                  await EzConfig.setString(savedTabKey, choice.name);
+
+                  currTab = choice;
                   if (mounted) setState(() {});
                 },
               ),
             ),
             EzConfig.separator,
 
-            // Shared rights I
-            rightsBlock(l10n.rvSharedRemainSilent),
-            rightsBlock(l10n.rvSharedDocument),
-
-            // Situational rights
-            populateTab(),
-
-            // Shared rights II
-            rightsBlock(l10n.rvSharedSign),
-            rightsBlock(l10n.rvSharedFingerprint),
-            rightsBlock(l10n.rvSharedLawyer),
+            // Core
+            EzFauxCarousel(
+              position: currTab.position,
+              delta: delta,
+              child: GestureDetector(
+                child: EzCol(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _rightsText(l10n.rvSharedRemainSilent),
+                    _rightsText(l10n.rvSharedDocument),
+                    ..._unique(),
+                    _rightsText(l10n.rvSharedSign),
+                    _rightsText(l10n.rvSharedFingerprint),
+                    _rightsText(l10n.rvSharedLawyer),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       );
 }
+
+Text _rightsText(String text) =>
+    Text('$text\n', textAlign: TextAlign.start, style: EzConfig.styles.bodyLarge);
